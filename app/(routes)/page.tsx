1f5c9cbbc42a2f74501426581886f3e28e673f7c
MohamedH1000 <mohammedhisham115@gmail.com>
2024-08-21 01:40:47 +0300

    fix the endpoints

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -7,19 +7,19 @@ import ProductList from "@/components/product-list";
 export const revalidate = 0;
 
 const HomePage = async () => {
-    const products = await getProducts({ isFeatured: true });
-    const billboard = await getBillboard("26960b7b-d1eb-44b5-91c9-3648662f99f9");
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("66c5193b91945b492bd488ed");
 
-    return (
-      <Container>
-        <div className="space-y-10 pb-10">
-          <Billboard data={billboard} />
-          <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-            <ProductList title="Featured Products" items={products} />
-          </div>
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
         </div>
-      </Container>
-    );
-}
- 
+      </div>
+    </Container>
+  );
+};
+
 export default HomePage;
